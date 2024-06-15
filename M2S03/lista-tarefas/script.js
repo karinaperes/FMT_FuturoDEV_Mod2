b176a61f8e24e4f4176e3ac988cf51b9caf246cf
karinaperes <karinaperesg@gmail.com>
2024-06-15 19:40:53 -0300

    Inclusão exercicio 05 semana 03 lista de tarefas

---
+++ M2S03/lista-tarefas/script.js
@@ -0,0 +1,45 @@
+document.getElementById('btn-add-item').addEventListener('click', addItem)
+document.addEventListener('DOMContentLoaded', loadLista) 
+    
+function addItem() {
+    const itemInput = document.getElementById('item-input')
+    const item = itemInput.value
+
+    if (item !== '') {
+        addItemLista(item)
+        saveItem(item)
+        itemInput.value = ''
+    } else {
+        alert('Insira uma tarefa')
+    }
+}
+
+function addItemLista(item) {
+    const lista = document.getElementById('lista')
+    const novoItem = document.createElement('li')
+    const checkbox = document.createElement('input')
+
+    checkbox.type = 'checkbox'
+    checkbox.className = 'checkbox'
+
+    const itemText = document.createElement('span')
+
+    itemText.textContent = item
+
+    novoItem.appendChild(checkbox)
+    novoItem.appendChild(itemText)
+    lista.appendChild(novoItem)
+}
+
+function saveItem(item) {
+    let itens = localStorage.getItem('itens') ? JSON.parse(localStorage.getItem('itens')) : []
+    itens.push(item)
+    localStorage.setItem('itens', JSON.stringify(itens))
+}
+
+function loadLista() {
+    let itens = localStorage.getItem('itens') ? JSON.parse(localStorage.getItem('itens')) : []
+    itens.forEach(item => {
+        addItemLista(item)
+    })
+}
