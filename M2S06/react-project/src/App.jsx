c651551be1fa6392db05e53eca7cbe7177453b30
karinaperes <karinaperesg@gmail.com>
2024-07-08 17:09:35 -0300

    Inclusão exercicios 06 e 07 semana 06, formulario de contato

--- M2S06/react-project/src/App.jsx
+++ M2S06/react-project/src/App.jsx
@@ -1,13 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Contato from './components/Contato'
+// import Introducao from './components/Introducao'
+// import Bicicletas from './components/Bicicletas'
 
 function App() {
   return (
     <>
       <Header></Header>
-      <div>
-        
-      </div>
+      
+      <Contato></Contato>      
       
       <Footer></Footer>
     </>
