aa95872e0e07da3818862cfb21f781a4b03f28d8
karinaperes <karinaperesg@gmail.com>
2024-07-22 18:52:17 -0300

    Inclusão exercicio 01 semana 08 site Zé Delivery

---
+++ M2S08/ze-delivery/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import '../Footer/Footer.css'
+// import { Link } from "react-router-dom"
+
+function Footer() {
+    return (
+        <>
+            <div className='footer-container'>
+                <div className='footer-info-site'>
+                    <div className='voltar-topo'>
+
+                    </div>
+                    <div className='footer-site-map flex-row'>
+                        <div className='logo-ze'>
+                            <img src="https://www.ze.delivery/_next/image?url=https%3A%2F%2Fcourier-images-web.imgix.net%2Fstatic%2Fimg%2Flogo-white-text.png%3Fauto%3Dcompress%2Cformat%26fit%3Dmax%26w%3D176%26h%3D56%26dpr%3D2%26fm%3Dpng&w=256&q=75" alt="Logo Zé Delivery" />
+                        </div>
+                        <div className='sobre-container'>
+                            <div className="sobre  flex-column gap-10">
+                                <h2>Sobre o Zé Delivery</h2>
+                                <a href="https://faq-consumidor-zedelivery.sprinklr.com" target="_blank" rel="noreferrer noopener">Me ajuda, Zé!</a>
+                                <a href="https://bit.ly/374UOmX" target="_blank" rel="noreferrer noopener">Trabalhe conosco</a>
+                                <p>Cidades Atendidas</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='parcerias'>
+                                <h2>Parcerias</h2>
+                                <p>Quero ser parceiro</p>
+                            </div>
+                            <div className='cartao-presente'>
+                                <h2>Cartão Presente</h2>
+                                <p>Comprar aqui</p>
+                            </div>
+
+
+                        </div>
+                        
+
+                        <div className='social'></div>
+                    </div>
+
+                </div>
+                <div className='footer-info'>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ M2S08/ze-delivery/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import '../Hero/Hero.css'
+
+function Hero() {
+    return (
+        <>
+            <div className="hero-container">
+
+            </div>
+        </>
+    )
+}
+
+export default Hero
