3bd9273acf07b04dd4698d5d5a794bdd2f579fe3
karinaperes <karinaperesg@gmail.com>
2024-07-29 12:20:24 -0300

    Melhorias hero

--- M2S08/ze-delivery/src/pages/Home/Home.jsx
+++ M2S08/ze-delivery/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import Hero from "../../components/Hero/Hero"
 import Header from "../../components/Header/Header"
 import Footer from "../../components/Footer/Footer"
+import Produtos from "../../components/Produtos/Produtos"
 
 
 function Home() {
     return (
         <>
             <Hero></Hero>
-            <Header></Header>
+            <Header></Header>            
+            <Produtos></Produtos>
             <Footer></Footer>
 
         </>
